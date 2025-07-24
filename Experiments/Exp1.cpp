560aa56147cba13da4ca8ceb930ac1a85bbd0f82
Aarushi1512006 <aarushiahluwaliajaipur@gmail.com>
2025-07-24 21:18:01 +0530

    Added implementation of stack using Static Array

---
+++ Experiments/Exp1.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+//Program 1
+//Impllement stack sing static array;
+int stack[100], top = -1;
+int size =10;
+bool isFull(){
+        return top==size-1;
+}
+bool isEmpty(){
+        return top==-1;
+}
+void push(int v){
+    if(isFull())
+        cout<<"Stack Overflow"<<endl;
+    else{
+        top++;
+        stack[top] = v;
+    }
+}
+int pop(){
+    if(isEmpty()){
+        cout<<"Stack Underflow"<<endl;
+        return 0;
+    }
+    else{
+        int v = stack[top];
+        top--;
+        cout<<v<<" is deleted"<<endl;
+        return v;
+    }
+}
+void display(){
+    for(int i=0;i<=top;i++){
+        cout<<stack[i]<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    push(60);
+    push(70);
+    push(80);
+    push(90);
+    pop();
+    display();
+}
